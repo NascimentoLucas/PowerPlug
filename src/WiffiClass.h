55490991335e3108c74f823415010f5c9c55b415
NascimentoLucas <nascimentol@outlook.com>
2020-02-06 02:52:46 -0300

    updated link

--- src/WiffiClass.h
+++ src/WiffiClass.h
@@ -1,16 +1,9 @@
 class WiffiObject{  
   //ATÉ QUE O NÚMERO DE TENTATIVAS EXPIRE (RESULTANDO EM WL_NO_SHIELD) OU QUE UMA CONEXÃO SEJA ESTABELECIDA
   //(RESULTANDO EM WL_CONNECTED)
-  
   int statusLed = LOW; //VARIÁVEL QUE ARMAZENA O ESTADO ATUAL DO LED (LIGADO / DESLIGADO)
   
   WiFiEspClient client;
-  inline void sendLine(WiFiEspClient client, char head[],int value){
-    //client.print(";");
-    //client.print(head);
-    client.println(value);
-    client.print(";");
-  }
 
   void sendHttpResponse(WiFiEspClient client){
       client.println("HTTP/1.1 200 OK"); //ESCREVE PARA O CLIENTE A VERSÃO DO HTTP
@@ -18,30 +11,21 @@ class WiffiObject{
       client.println("");
       client.println("<!DOCTYPE HTML>"); //INFORMA AO NAVEGADOR A ESPECIFICAÇÃO DO HTML
       client.println("<html>"); //ABRE A TAG "html"
-      client.println("<head>"); //ABRE A TAG "head"
-      client.println("<link rel='icon' type='image/png' href='https://blogmasterwalkershop.com.br/arquivos/artigos/sub_wifi/icon_mws.png'/>"); //DEFINIÇÃO DO ICONE DA PÁGINA
-      client.println("<link rel='stylesheet' type='text/css' href='https://blogmasterwalkershop.com.br/arquivos/artigos/sub_wifi/webpagecss.css' />"); //REFERENCIA AO ARQUIVO CSS (FOLHAS DE ESTILO)
-      client.println("<title>MasterWalker Shop - Modulo WiFi ESP8266 para Arduino</title>"); //ESCREVE O TEXTO NA PÁGINA
-      client.println("</head>"); //FECHA A TAG "head"
-      
-      //AS LINHAS ABAIXO CRIAM A PÁGINA HTML
-      client.println("<body>"); //ABRE A TAG "body"
-      client.println("<img alt='masterwalkershop' src='https://blogmasterwalkershop.com.br/arquivos/artigos/sub_wifi/logo_mws.png' height='156' width='700' />"); //LOGO DA MASTERWALKER SHOP
-      client.println("<p style='line-height:2'><font>Modulo WiFi ESP8266 para Arduino</font></p>"); //ESCREVE O TEXTO NA PÁGINA
-      client.println("<font>ESTADO ATUAL DO LED</font>"); //ESCREVE O TEXTO NA PÁGINA
-      
+      client.println("<head>"); //ABRE A TAG "head" 
+      client.println("<script type=\"text/javascript\" src=\"http://devlucasnascimento.com.br/Arduino/loadHtml.js \"></script>");               
+      client.println("</head>"); // FEACHA A TAG "head"
+      client.println("<body>"); 
+      client.println("<div id=\"content\"></div>");
       if (statusLed == HIGH){ //SE VARIÁVEL FOR IGUAL A HIGH (1), FAZ
         client.println("<p style='line-height:0'><font color='green'>LIGADO</font></p>"); //ESCREVE "LIGADO" NA PÁGINA
         client.println("<a href=\"/L\">APAGAR</a>"); //COMANDO PARA APAGAR O LED (PASSA O PARÂMETRO /L)
+        
       }else{ //SENÃO, FAZ
-        if (statusLed == LOW){ //SE VARIÁVEL FOR IGUAL A LOW (0), FAZ
+       //SE VARIÁVEL FOR IGUAL A LOW (0), FAZ
         client.println("<p style='line-height:0'><font color='red'>DESLIGADO</font></p>"); //ESCREVE "DESLIGADO" NA PÁGINA
         client.println("<a href=\"/H\">ACENDER</a>"); //COMANDO PARA ACENDER O LED (PASSA O PARÂMETRO /H)
-        }
       }
-      client.println("<hr />"); //TAG HTML QUE CRIA UMA LINHA NA PÁGINA
-      client.println("<hr />"); //TAG HTML QUE CRIA UMA LINHA NA PÁGINA
-      client.println("</body>"); //FECHA A TAG "body"
+      client.println("</body>"); 
       client.println("</html>"); //FECHA A TAG "html"
       
       delay(1); //INTERVALO DE 1 MILISSEGUNDO
@@ -64,7 +48,9 @@ class WiffiObject{
     
             //IDENTIFICA O FIM DA REQUISIÇÃO HTTP E ENVIA UMA RESPOSTA
             if(buf.endsWith("\r\n\r\n")) {
-              sendHttpResponse(client);
+              
+                sendHttpResponse(client);
+              
               break;
             }
             if(buf.endsWith("GET /H")){ //SE O PARÂMETRO DA REQUISIÇÃO VINDO POR GET FOR IGUAL A "H", FAZ 
@@ -79,7 +65,7 @@ class WiffiObject{
             }
           }
         }
-
+      
         client.stop(); //FINALIZA A REQUISIÇÃO HTTP E DESCONECTA O CLIENTE
       }
       else{              
