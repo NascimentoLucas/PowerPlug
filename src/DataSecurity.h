5bbad66d0480640b14ea79fa5939738d533be246
Nascimento Lucas <nascimentol@outlook.com>
2020-02-13 00:28:30 -0300

    added DataSecurity
    this file is in the gitignore for security

---
+++ src/DataSecurity.h
@@ -0,0 +1,12 @@
+class DataSecurityObject{
+    public: 
+    char ssid[18] = "wiffiname";
+    char pass[14] = "password";
+    char url[14] = "192.168.0.2";
+    int ip[4] = {192, 168, 0, 2};
+
+    DataSecurityObject(int i)
+    {
+
+    }
+};
