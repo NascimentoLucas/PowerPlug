93846a937b0e4c4180a8ecfef2e846b98b2740f4
Nascimento Lucas <nascimentol@outlook.com>
2020-02-18 17:17:37 -0300

    Update DataSecurity.h

--- src/DataSecurity.h
+++ src/DataSecurity.h
@@ -1,14 +1,12 @@
-diff a/src/DataSecurity.h b/src/DataSecurity.h	(rejected hunks)
-@@ -1,12 +0,0 @@
--class DataSecurityObject{
--    public: 
--    char ssid[18] = "wiffiname";
--    char pass[14] = "password";
--    char url[14] = "192.168.0.2";
--    int ip[4] = {192, 168, 0, 2};
--
--    DataSecurityObject(int i)
--    {
--
--    }
--};
+class DataSecurityObject{
+    public: 
+    char ssid[18] = "wiffiname";
+    char pass[14] = "password";
+    char url[14] = "192.168.0.2";
+    int ip[4] = {192, 168, 0, 2};
+
+    DataSecurityObject(int i)
+    {
+
+    }
+};
